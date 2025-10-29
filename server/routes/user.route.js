6ea7cdd5ce3195ef3472534573b683478cce23ef
Anand Jha <anandjha8722@gmail.com>
2025-10-29 18:46:17 +0530

    authentication route done

---
+++ server/routes/user.route.js
@@ -0,0 +1,93 @@
+import express from "express";
+import user from "../models/user.schema.js";
+import jwt from "jsonwebtoken";
+
+const router = express.Router();
+
+//@route POST /api/users/register
+//register user
+//@access Public
+router.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    // Registration Logic
+    let User = await user.findOne({ email });
+    if (User) {
+      return res.status(400).json({
+        message: "user already exists",
+      });
+    }
+    User = new user({ name, email, password });
+    await User.save();
+
+    const payload = { user: { _id: User._id, role: User.role } };
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: "2h" },
+      (err, token) => {
+        if (err) throw err;
+        // send the user and token in response
+        res.status(201).json({
+          user: {
+            _id: User._id,
+            name: User.name,
+            email: User.email,
+            role: User.role,
+          },
+          token,
+        });
+      }
+    );
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("server Error", err);
+  }
+});
+
+//@route POST /api/users/login
+//desc Authenticate user
+//@access Public
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    // Find user by email
+    let existingUser = await user.findOne({ email });
+    if (!existingUser) {
+      return res.status(400).json({
+        message: "Invalid Credentials",
+      });
+    }
+    const isMatch = await existingUser.matchPassword(password);
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid Credentials" });
+
+
+    const payload = { user: { _id: existingUser._id, role: existingUser.role } };
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: "2h" },
+      (err, token) => {
+        if (err) throw err;
+        // send the user and token in response
+        res.json({
+          existingUser: {
+            _id: existingUser._id,
+            name: existingUser.name,
+            email: existingUser.email,
+            role: existingUser.role,
+          },
+          token,
+        });
+      }
+    );
+  } catch (err) {
+    console.log(err)
+    res.status(500).send('server Error while Login')
+  }
+});
+
+export default router;
--- server/server.js
+++ server/server.js
@@ -2,16 +2,25 @@ import express from 'express'
 import cors from "cors"
 import dotenv from "dotenv"
 dotenv.config();
+import connectDB from './dbs/db.js';
+import userRoute from '../server/routes/user.route.js'
+
 
 const app = express()              
 app.use(express.json())
 app.use(cors())
 
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT || 7000 ;
+// connect to MongoDB
+
+connectDB()
+
 app.get("/",(req,res)=>{
     res.send("Welcome to Cartopia API!!")
 
 })
+
+app.use("/api/users",userRoute);
 app.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}`)
 })
